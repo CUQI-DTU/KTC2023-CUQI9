73bad35444eba56a4ef561ec71d1beec14076fd7
rasmuskleist <kleist.rasmus@gmail.com>
2023-09-12 20:11:08 +0200

    Add outline of complete electrode model class

---
+++ ktc/model.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class CompleteElectrodeModel:
+    def __init__(self, injection, **kwargs):
+        self.injection = injection
+        self.sigmamin = kwargs.get("sigmamin", 1e-9)
+        self.sigmamax = kwargs.get("sigmamax", 1e9)
+        self.zmin = kwargs.get("zmin", 1e-6)
+        self.zmax = kwargs.get("zmax", 1e6)
+
+    def solve(self, sigma, z):
+        sigma = np.clip(sigma, self.sigmamin, self.sigmamax)
+        z = np.clip(z, self.zmin, self.zmax)
+
+    def jacobian(self, sigma, z):
+        pass
