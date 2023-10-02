85da40d95dcd12a0d3f745931b67b4c095980764
jakobtkn <s194325@student.dtu.dk>
2023-10-02 12:37:58 +0200

    Forward Model Interface outline

--- ktc/models.py
+++ ktc/models.py
@@ -1,4 +1,37 @@
+import abc
+import typing
+
 import numpy as np
+from numpy.typing import ArrayLike
+
+
+class ForwardModel(metaclass=abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (
+            hasattr(subclass, "jacobian")
+            and callable(subclass.jacobian)
+            and hasattr(subclass, "neumann_to_dirichlet")
+            and callable(subclass.neumann_to_dirichlet)
+            and hasattr(subclass, "jacobian")
+            and callable(subclass.jacobian)
+            or NotImplemented
+        )
+
+    @abc.abstractmethod
+    def neumann_to_dirichlet(self, current_injection: ArrayLike) -> ArrayLike:
+        """Compute Neumann to Dirichlet map"""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def jacobian(self, current_injection: ArrayLike) -> ArrayLike:
+        """Return Jacobian"""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def poisson(self, pertubation: ArrayLike, current_injection: ArrayLike) -> ArrayLike:
+        """Return solution to generalized Poisson problem"""
+        raise NotImplementedError
 
 class CompleteElectrodeModel:
     def __init__(self, injection, **kwargs):
