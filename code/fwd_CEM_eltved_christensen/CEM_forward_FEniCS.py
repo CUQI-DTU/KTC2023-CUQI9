7613634f219b9bb90f6fb67179d80b761d512a3d
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-10-13 12:46:33 +0200

    Autoformat

--- code/fwd_CEM_eltved_christensen/CEM_forward_FEniCS.py
+++ code/fwd_CEM_eltved_christensen/CEM_forward_FEniCS.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 """
 Note by Amal Alghamdi: This code is copied from the project report: Depth 
 Dependency in Electrical Impedance Tomography with the Complete 
@@ -17,22 +17,22 @@ from utils import *
 import scipy.io as io
 from scipy.interpolate import RegularGridInterpolator
 
-case_name = 'case2' #'case1' , case3', 'case4', 'case_ref'
+case_name = 'case2'  # 'case1' , case3', 'case4', 'case_ref'
 KTC23_dir = './KTC23_data/'
 
 L = 32
 # Define vector of contact impedance
-z =  10e-6##0.1 # Impedence 
+z = 10e-6  # 0.1 # Impedence
 Z = []
-for i in range (L):
-    Z.append (z)
+for i in range(L):
+    Z.append(z)
 
 # Define domain
-R = 1 # radius of circle
-n = 300#300 # 300 # number o f polygons to approximate circle
+R = 1  # radius of circle
+n = 300  # 300 # 300 # number o f polygons to approximate circle
 F = 50  # 50 # fineness of mesh
-mesh = generate_mesh(Circle(Point( 0, 0), R, n) ,F) # generate mesh
-N = mesh.num_entities( 2 )
+mesh = generate_mesh(Circle(Point(0, 0), R, n), F)  # generate mesh
+N = mesh.num_entities(2)
 subdomains = build_subdomains(L, mesh)
 
 xdmf = XDMFFile(case_name+"file_sub.xdmf")
@@ -40,7 +40,7 @@ xdmf.write(subdomains)
 V, dS = build_spaces(mesh, L, subdomains)
 
 high_conductivity = 1e1
-low_conductivity =  1e-2
+low_conductivity = 1e-2
 background_conductivity = 0.8
 
 Imatr = io.loadmat(KTC23_dir+"ref.mat")["Injref"]
@@ -74,52 +74,56 @@ elif case_name == 'case_ref':
 else:
     raise Exception("unknown case")
 
-#%%
+# %%
 # Define conductivity
-class inclusion( UserExpression ):
-    def __init__(self, phantom , **kwargs):
+
+
+class inclusion(UserExpression):
+    def __init__(self, phantom, **kwargs):
         super().__init__(**kwargs)
         x_grid = np.linspace(-1, 1, 256)
         y_grid = np.linspace(-1, 1, 256)
-        self._interpolater = RegularGridInterpolator((x_grid, y_grid), phantom, method="nearest") 
+        self._interpolater = RegularGridInterpolator(
+            (x_grid, y_grid), phantom, method="nearest")
 
-    def eval( self , values , x ) :
-        values[ 0 ] = self._interpolater([ x [ 0 ] , x [ 1 ] ])
+    def eval(self, values, x):
+        values[0] = self._interpolater([x[0], x[1]])
+
 
 phantom_float = np.zeros(phantom.shape)
 phantom_float[phantom == 0] = background_conductivity
-phantom_float[np.isclose(phantom, 1, rtol = 0.01)] = low_conductivity 
+phantom_float[np.isclose(phantom, 1, rtol=0.01)] = low_conductivity
 phantom_float[phantom == 2] = high_conductivity
 
 plt.figure()
 im = plt.imshow(phantom_float)
-plt.colorbar(im) #norm= 'log'
+plt.colorbar(im)  # norm= 'log'
 plt.savefig(case_name+"_ph_mod.png")
 
-my_inclusion = inclusion(phantom_float, degree= 1) 
-#%%
+my_inclusion = inclusion(phantom_float, degree=1)
+# %%
 
 # Define H1 room
-H1=FunctionSpace(mesh ,'CG' , 1)
+H1 = FunctionSpace(mesh, 'CG', 1)
 
-# Loop over current patterns 
-num_inj = 76 # Number of injection pattern
+# Loop over current patterns
+num_inj = 76  # Number of injection pattern
 num_inj_tested = 76
 
 B = build_b(my_inclusion, Z, V, dS, L)
-Q = np.zeros((L,num_inj))
-Diff= np.zeros((L-1,num_inj)) 
+Q = np.zeros((L, num_inj))
+Diff = np.zeros((L-1, num_inj))
 q_list = []
 
 for i in range(num_inj)[:num_inj_tested]:
     print("injection pattern"+str(i))
-    Q_i , q = solver(Imatr[:,i], B, V, dS, L)
+    Q_i, q = solver(Imatr[:, i], B, V, dS, L)
     q_list.append(q)
 
     Q[:, i] = Q_i
     Diff[:, i] = np.diff(Q_i)
 
-#%% Plot potential solution for each injection pattern
+# %% Plot potential solution for each injection pattern
 for i, q in enumerate(q_list):
     plt.figure()
     h_func = Function(H1)
@@ -143,11 +147,13 @@ plt.plot(-Uel_ref[:num_inj_tested*31])
 plt.savefig(case_name+"data_flattened"+str(L)+"_"+str(i)+".png")
 
 plt.figure()
-plt.plot(-Uel_ref[:num_inj_tested*31].flatten()- Diff.flatten(order='F')[:num_inj_tested*31])
+plt.plot(-Uel_ref[:num_inj_tested*31].flatten() -
+         Diff.flatten(order='F')[:num_inj_tested*31])
 plt.savefig(case_name+"data_model_diff"+str(L)+"_"+str(i)+".png")
-# %% Error and model data 
+# %% Error and model data
 plt.figure()
-plt.plot(-Uel_ref[:num_inj_tested*31].flatten()- Diff.flatten(order='F')[:num_inj_tested*31], label= 'Error')
-plt.plot(Diff.flatten(order='F')[:num_inj_tested*31], label= 'Model output')
+plt.plot(-Uel_ref[:num_inj_tested*31].flatten() -
+         Diff.flatten(order='F')[:num_inj_tested*31], label='Error')
+plt.plot(Diff.flatten(order='F')[:num_inj_tested*31], label='Model output')
 plt.legend()
 plt.savefig(case_name+"data_diff_and_model.png"+str(L)+"_"+str(i)+".png")
--- code/fwd_CEM_eltved_christensen/utils.py
+++ code/fwd_CEM_eltved_christensen/utils.py
@@ -5,110 +5,110 @@ Electrode Model by Anders Eltved and Nikolaj Vestbjerg Christensen (Appendix D.5
 modifications are made.
 """
 
-import numpy as np 
+import numpy as np
 from dolfin import *
 
+
 def build_subdomains(L, mesh):
-  def twopiarctan( x ) :
-    val=np.arctan2( x [ 1 ] , x [ 0 ] )
-    if val <0:
-      val=val+2*np.pi
-    return val
-  
-  e_l=np.pi /L
-  d_e=2*np.pi /L - e_l
+    def twopiarctan(x):
+        val = np.arctan2(x[1], x[0])
+        if val < 0:
+            val = val+2*np.pi
+        return val
 
-  # Define subdomain mesh
-  subdomains = MeshFunction("size_t", mesh, mesh.topology().dim()-1, 0)
+    e_l = np.pi / L
+    d_e = 2*np.pi / L - e_l
 
- # Define subdomains
-  class e(SubDomain) :
-    def inside( self , x , on_boundary ) :
-      theta=twopiarctan ( x )
-      #print "theta inside", theta
-      if theta1 > theta2:
-        return on_boundary and ((theta>=0 
-          and theta<=theta2) or (theta>=theta1 
-          and theta<=2*np.pi) )        
-      return on_boundary and theta>=theta1 \
-        and theta<=theta2
-      #return  theta>=theta1 and theta<=theta2
+    # Define subdomain mesh
+    subdomains = MeshFunction("size_t", mesh, mesh.topology().dim()-1, 0)
 
-  for i in range ( 1 , L+1) :
-    shift_theta = np.pi/2 - np.pi/(2*L)
-    #print "shift_theta", shift_theta
-    #print L
-    theta1 =np.mod( ( i -1) *( e_l+d_e ) + shift_theta, 2*np.pi)
-    theta2 =np.mod( theta1+e_l , 2*np.pi) 
-    #print i
-    #print theta1
-    #print theta2
-    e1 = e ( ) # create instance
-    e1 .mark( subdomains , i ) # mark subdomain
+   # Define subdomains
+    class e(SubDomain):
+        def inside(self, x, on_boundary):
+            theta = twopiarctan(x)
+            # print "theta inside", theta
+            if theta1 > theta2:
+                return on_boundary and ((theta >= 0
+                                         and theta <= theta2) or (theta >= theta1
+                                                                  and theta <= 2*np.pi))
+            return on_boundary and theta >= theta1 \
+                and theta <= theta2
+            # return  theta>=theta1 and theta<=theta2
 
-  return subdomains
+    for i in range(1, L+1):
+        shift_theta = np.pi/2 - np.pi/(2*L)
+        # print "shift_theta", shift_theta
+        # print L
+        theta1 = np.mod((i - 1) * (e_l+d_e) + shift_theta, 2*np.pi)
+        theta2 = np.mod(theta1+e_l, 2*np.pi)
+        # print i
+        # print theta1
+        # print theta2
+        e1 = e()  # create instance
+        e1 .mark(subdomains, i)  # mark subdomain
+
+    return subdomains
+
 
 def build_spaces(mesh, L, subdomains):
-  R = FunctionSpace (mesh , "R" , 0 )
-  H1 = FunctionSpace (mesh , "CG" , 1 )
+    R = FunctionSpace(mesh, "R", 0)
+    H1 = FunctionSpace(mesh, "CG", 1)
 
-  spacelist = None
+    spacelist = None
 
-  for i in range ( 1 , L+1) :
+    for i in range(1, L+1):
 
-    if i==1:
-      spacelist=R.ufl_element()
-    else:
-      spacelist*=R.ufl_element()
-  
-  spacelist*=H1.ufl_element()
-  spacelist*=R.ufl_element()
+        if i == 1:
+            spacelist = R.ufl_element()
+        else:
+            spacelist *= R.ufl_element()
 
-  # Create function space
-  V = FunctionSpace(mesh, spacelist)
+    spacelist *= H1.ufl_element()
+    spacelist *= R.ufl_element()
 
-  # Define new measures associated with the boundaries
-  dS = Measure ( 'ds' , domain=mesh ) [ subdomains ]
+    # Create function space
+    V = FunctionSpace(mesh, spacelist)
 
-  return V, dS
-   
+    # Define new measures associated with the boundaries
+    dS = Measure('ds', domain=mesh)[subdomains]
+
+    return V, dS
+
+
 def build_b(sigma, Z, V, dS, L):
 
-  # Define trial and test functions
-  u = TrialFunction(V)
-  v = TestFunction(V)
+    # Define trial and test functions
+    u = TrialFunction(V)
+    v = TestFunction(V)
 
-  B = sigma * inner ( nabla_grad (u [L ] ) , nabla_grad ( v [L ] ) ) *dx
+    B = sigma * inner(nabla_grad(u[L]), nabla_grad(v[L])) * dx
 
-  for i in range (L) :
-    B += 1/Z [ i ] * ( u [L]-u [ i ] )*( v [L]-v [ i ] ) *dS( i +1)
-    B += ( v [L+1]*u [ i ] / assemble (1*dS ( i+1 ) ) ) *dS ( i+1 )
-    B += (u [L+1]*v [ i ] / assemble (1*dS ( i+1 ) ) ) *dS ( i+1 )
+    for i in range(L):
+        B += 1/Z[i] * (u[L]-u[i])*(v[L]-v[i]) * dS(i + 1)
+        B += (v[L+1]*u[i] / assemble(1*dS(i+1))) * dS(i+1)
+        B += (u[L+1]*v[i] / assemble(1*dS(i+1))) * dS(i+1)
 
-  return assemble(B)
-
-
-
-def solver(I, B, V, dS, L):# sigma ,L, I , Z ,mesh, subdomains ) 
- # def 2 pi function
-
-  # Define trial and test functions
-  u = TrialFunction(V)
-  v = TestFunction(V)
+    return assemble(B)
 
 
-  f = 0*dS ( 1 )
+def solver(I, B, V, dS, L):  # sigma ,L, I , Z ,mesh, subdomains )
+   # def 2 pi function
 
-  for i in range (L) :
-    f += ( I [ i ] * v [ i ] / assemble (1*dS ( i+1 ) ) ) *dS ( i+1)
+    # Define trial and test functions
+    u = TrialFunction(V)
+    v = TestFunction(V)
 
-  rhs = assemble(f)
+    f = 0*dS(1)
 
+    for i in range(L):
+        f += (I[i] * v[i] / assemble(1*dS(i+1))) * dS(i+1)
 
-  # Compute solution
-  q = Function (V)
-  solve(B, q.vector(), rhs)
+    rhs = assemble(f)
 
-  Q = q.vector().get_local()[:L]
+    # Compute solution
+    q = Function(V)
+    solve(B, q.vector(), rhs)
 
-  return Q, q
+    Q = q.vector().get_local()[:L]
+
+    return Q, q
