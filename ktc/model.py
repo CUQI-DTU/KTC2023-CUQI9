9256e8b3bcdd42f9dd605cc3375c3411afa774e4
jakobtkn <s194325@student.dtu.dk>
2023-10-20 12:06:05 +0200

    reconstruction mesh

--- ktc/model.py
+++ ktc/model.py
@@ -1,9 +1,9 @@
 import abc
 import meshio
+import gmsh
 
 import numpy as np
 from numpy.typing import ArrayLike
-
 
 class ForwardModel(metaclass=abc.ABCMeta):
     @classmethod
@@ -35,15 +35,27 @@ class ForwardModel(metaclass=abc.ABCMeta):
 
 
 class FenicsForwardModel(ForwardModel):
-    def __init__(self) -> None:
+    def __init__(self, number_of_electrodes) -> None:
+        self.number_of_electrodes = 32
+        
         super().__init__()
 
     def solve(self, current_injection: ArrayLike) -> ArrayLike:
         """Compute Neumann to Dirichlet map"""
         pass
 
-    def jacobian(self, current_injection: ArrayLike) -> ArrayLike:
+    def jacobian(self, mesh, current_injections) -> ArrayLike:
         """Return jacobian"""
+        current_injections = np.identity(self.number_of_electrodes)
+        N = len(mesh.points)
+        J = np.zeros(self.number_of_injections*self.number_of_electrodes, N)
+        
+        for n,face in enumerate(mesh.faces):
+            #TODO: Compute conductivity map with specific 
+            face_pertubation = 0
+            J[:,n] = self.poisson(face_pertubation, current_injections)
+
+        
         pass
 
     def poisson(
--- ktc/reconstruction.py
+++ ktc/reconstruction.py
@@ -1,26 +1,33 @@
 import numpy as np
-import meshio
+import pygmsh
 
-from ktc.model import ForwardModel
+from model import ForwardModel
 from numpy.typing import ArrayLike
 
 
 class SeriesReversion:
-    def __init__(self, model: ForwardModel, mesh, **kwargs):
+    def __init__(self, model: ForwardModel, **kwargs):
         self.model = model
-        self.mesh = mesh
         self.sigma_ref = kwargs.get("sigma", 1)
         self.data_ref = kwargs.get("data", 0)  # Compute default using fwd model
+        self.rec_mesh = self.generate_reconstruction_mesh(boundary_gap = 0.1, mesh_size = 0.2)
+        
+    def generate_reconstruction_mesh(self, boundary_gap, mesh_size):
+        radius = 1 - boundary_gap
+        with pygmsh.geo.Geometry() as geom:
+            geom.add_circle([0.0, 0.0], radius, mesh_size=mesh_size)
+            return geom.generate_mesh()
+
 
     def reconstruct(self, current_injections: ArrayLike, data: ArrayLike):
         J = self.model.jacobian(self.sigma_ref, self.mesh)
 
         F1 = np.linalg.solve(J, (data - self.data_ref))
 
-        u, _ = self.model.solve(current_injections)
+        # u, _ = self.model.solve(current_injections)
 
-        h = -self.model.poisson(F1, current_injections, u)
-        v = self.model.poisson(F1, current_injections, h)
-        F2 = np.linalg.solve(J, v)
+        # h = -self.model.poisson(F1, current_injections, u)
+        # v = self.model.poisson(F1, current_injections, h)
+        # F2 = np.linalg.solve(J, v)
 
-        return F1 + F2
+        return F1
