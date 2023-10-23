dd8157402d83a247b037f055c21d1ad0c77bd3b4
jakobtkn <s194325@student.dtu.dk>
2023-10-23 13:07:18 +0200

    Changed reconstruction to take mesh as input

--- ktc/reconstruction.py
+++ ktc/reconstruction.py
@@ -3,21 +3,13 @@ import pygmsh
 
 from model import ForwardModel
 from numpy.typing import ArrayLike
-
-
 class SeriesReversion:
-    def __init__(self, model: ForwardModel, **kwargs):
+    def __init__(self, model: ForwardModel, mesh, **kwargs):
         self.model = model
+        self.mesh = mesh
         self.sigma_ref = kwargs.get("sigma", 1)
         self.data_ref = kwargs.get("data", 0)  # Compute default using fwd model
         self.rec_mesh = self.generate_reconstruction_mesh(boundary_gap = 0.1, mesh_size = 0.2)
-        
-    def generate_reconstruction_mesh(self, boundary_gap, mesh_size):
-        radius = 1 - boundary_gap
-        with pygmsh.geo.Geometry() as geom:
-            geom.add_circle([0.0, 0.0], radius, mesh_size=mesh_size)
-            return geom.generate_mesh()
-
 
     def reconstruct(self, current_injections: ArrayLike, data: ArrayLike):
         J = self.model.jacobian(self.sigma_ref, self.mesh)
