6f0a089c6ce2dd4da0e94de97a51ca6fa0c4586e
jakobtkn <s194325@student.dtu.dk>
2023-10-23 15:03:07 +0200

    frame operator

--- ktc/reconstruction.py
+++ ktc/reconstruction.py
@@ -10,10 +10,14 @@ class SeriesReversion:
         self.sigma_ref = kwargs.get("sigma", 1)
         self.data_ref = kwargs.get("data", 0)  # Compute default using fwd model
         self.rec_mesh = self.generate_reconstruction_mesh(boundary_gap = 0.1, mesh_size = 0.2)
-
+        
+    def frame_operator(self, current_injections):
+        S = current_injections @ current_injections.T
+        return S
+        
     def reconstruct(self, current_injections: ArrayLike, data: ArrayLike):
         J = self.model.jacobian(self.sigma_ref, self.mesh)
-
+    
         F1 = np.linalg.solve(J, (data - self.data_ref))
 
         # u, _ = self.model.solve(current_injections)
