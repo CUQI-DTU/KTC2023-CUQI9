e568c2ad3f39da9b2f054290cf69823f619331ff
jakobtkn <s194325@student.dtu.dk>
2023-10-25 19:44:06 +0200

    save output

--- test_script.py
+++ test_script.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
+from datetime import datetime
 import glob
 
 
@@ -30,16 +31,18 @@ for idx, fileName in enumerate(DATA_FILES):
     impedance = np.full(electrode_count, 1e-6)
     radius = 1
     
-    mesh, subdomains = create_disk_mesh(radius, electrode_count, polygons=150, fineness=20)
+    mesh, subdomains = create_disk_mesh(radius, electrode_count, polygons=200, fineness=20)
     forward_model = FenicsForwardModel(mesh, subdomains,electrode_count, impedance, background_conductivity)
     
     boundary_gap = 0.1
     reconstruction_mesh, _ = create_disk_mesh(radius - boundary_gap, electrode_count, polygons=50, fineness=7)
 
-    W = FunctionSpace(reconstruction_mesh, "DG", 0)
-    series_reversion = SeriesReversion(forward_model, reconstruction_mesh, CURRENT_INJECTIONS, W)
+    # W = FunctionSpace(reconstruction_mesh, "DG", 0)
+    # series_reversion = SeriesReversion(forward_model, reconstruction_mesh, CURRENT_INJECTIONS, W)
  
-    F = series_reversion.reconstruct(data.voltages)
-    series_reversion.solution_plot(F)
-    plt.show()
+    # F = series_reversion.reconstruct(data.voltages)
+    # series_reversion.solution_plot(F)
+    now = datetime.now()
+    file_name = now.strftime("%d_time_%H_%M.png")
+    plt.savefig(file_name)
     
