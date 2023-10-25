9798e3d9f59a9720e75ff36e81102bd776e90953
jakobtkn <s194325@student.dtu.dk>
2023-10-25 11:49:14 +0200

    test_script update

--- ktc/reconstruction.py
+++ ktc/reconstruction.py
@@ -3,20 +3,21 @@ import numpy as np
 from ktc.model import FenicsForwardModel
 class SeriesReversion:
     # Current injections: each row is a unique injection pattern
-    def __init__(self, model, mesh, current_injections):
+    def __init__(self, model, mesh, current_injections, W):
         self.model = model
         self.mesh = mesh
         
-        #self.u = []
-        #self.U = []
-        #for current_injection in current_injections:
-        #    ui, Ui = self.model.solve_forward(current_injection)
-        #    self.u.append(ui)
-        #    self.U.append(Ui)
+        self.u = []
+        self.U = []
+        for current_injection in current_injections:
+           ui, Ui = self.model.solve_forward(current_injection)
+           self.u.append(ui)
+           self.U.append(Ui)
             
         # Construct conoical dual frame
-        ## TODO: Solve using lsq (and verify it is correct)
-        #self.dual_frame = np.linalg.solve(current_injections @ current_injections.T, #current_injections)
+        # TODO: Solve using lsq (and verify it is correct)
+        self.dual_frame = np.linalg.solve(current_injections @ current_injections.T,current_injections)
+        self.W = W
         
     def reconstruct(self, data):
         J = self.model.gradient(self.sigma_ref, self.mesh)
--- test_script.py
+++ test_script.py
@@ -4,19 +4,17 @@ import scipy as sp
 import glob
 
 
-from dolfin import plot
+from dolfin import plot, project, interpolate, FunctionSpace
 
 from ktc.model import create_disk_mesh, FenicsForwardModel
 from ktc.reconstruction import SeriesReversion
 
 REFERENCE = sp.io.loadmat("data/TrainingData/ref.mat")
-CURRENT_INJECTIONS = REFERENCE["Injref"]
+CURRENT_INJECTIONS = REFERENCE["Injref"].T
 DATA_FILES = sorted(glob.glob("data/TrainingData/data1.mat"))
 TRUTH_FILES = sorted(glob.glob("data/GroundTruths/true*.mat"))
 
 for idx, fileName in enumerate(DATA_FILES):
-    print(idx)
-    print(fileName)
     matdict = sp.io.loadmat(fileName)
     uel = matdict.get("Uel")
     # TRUTH = sp.io.loadmat("data/GroundTruths/true" + idx + ".mat")
@@ -26,20 +24,34 @@ for idx, fileName in enumerate(DATA_FILES):
     impedance = np.full(electrode_count, 1e-6)
     radius = 1
     
-    mesh, subdomains = create_disk_mesh(radius, electrode_count, polygons=300, fineness=50)
+    mesh, subdomains = create_disk_mesh(radius, electrode_count, polygons=300, fineness=2)
     forward_model = FenicsForwardModel(mesh, subdomains,electrode_count, impedance, background_conductivity)
     
     boundary_gap = 0.1
-    reconstruction_mesh, _ = create_disk_mesh(radius - boundary_gap, electrode_count, polygons=100, fineness=10)
-    series_reversion = SeriesReversion(forward_model, reconstruction_mesh, background_conductivity)
+    reconstruction_mesh, _ = create_disk_mesh(radius - boundary_gap, electrode_count, polygons=5, fineness=1)
     
-    forward_model._characteristic_function(reconstruction_mesh, 0)
+    W = FunctionSpace(mesh, "DG", 0)
+    series_reversion = SeriesReversion(forward_model, reconstruction_mesh, CURRENT_INJECTIONS, W)
     
-    u, U = forward_model.solve_forward(CURRENT_INJECTIONS[:,0])
-    print((reconstruction_mesh.num_cells()))
-    print((reconstruction_mesh.num_vertices()))
-    print(len(reconstruction_mesh.coordinates()))
-    # plot(u)
+    H = forward_model._interior_potential_space()
+    coeffs = forward_model._compute_coefficients(series_reversion.u, W, H)
+    # print(coeffs)
+    # chi = forward_model._basis(5,W,H)
+    # print(chi)
+    # plot(chi)
+    
+    # chi = forward_model._characteristic_function(reconstruction_mesh, 0)
+    # H = forward_model._interior_potential_space()
+    
+    
+    # u, U = forward_model.solve_forward(CURRENT_INJECTIONS[:,0])
+    # plot(reconstruction_mesh)
+    # # print((reconstruction_mesh.num_cells()))
+    # # print((reconstruction_mesh.num_vertices()))
+    # print(reconstruction_mesh.coordinates())
+    # print(len(reconstruction_mesh.coordinates()))
+    # # plot(u)
+
     # plt.show()
     
     
