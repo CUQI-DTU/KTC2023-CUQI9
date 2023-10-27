ddd76da7d51e80c698842c1e1932f2cfa93a7d1c
jakobtkn <s194325@student.dtu.dk>
2023-10-27 16:06:48 +0200

    data reader supports reference

--- ktc/data_reader.py
+++ ktc/data_reader.py
@@ -2,15 +2,23 @@ import scipy as sp
 import numpy as np
 
 CURRENT_INJECTION_KEY = "Inj"
+CURRENT_INJECTION_KEY_REF = "Injref"
 VOLTAGE_DIFFERENCE_KEY = "Uel"
-
+VOLTAGE_DIFFERENCE_KEY_REF = "Uelref"
 class DataReader:
     def __init__(self, matdict):
-        self.current_injections = matdict[CURRENT_INJECTION_KEY].T
+        Uel = None
+        try:
+            self.current_injections = matdict[CURRENT_INJECTION_KEY].T
+            Uel = matdict[VOLTAGE_DIFFERENCE_KEY]
+        except:
+            self.current_injections = matdict[CURRENT_INJECTION_KEY_REF].T
+            Uel = matdict[VOLTAGE_DIFFERENCE_KEY_REF]
+            
         self.injection_count, self.electrode_count = self.current_injections.shape
 
         diff_shape = (self.electrode_count-1, self.injection_count)
-        voltage_diff = np.reshape(matdict[VOLTAGE_DIFFERENCE_KEY],diff_shape,order="F").T
+        voltage_diff = np.reshape(Uel,diff_shape,order="F").T
         self.voltages = self._undifference(voltage_diff)
         
     def _undifference(self, voltage_differences):
