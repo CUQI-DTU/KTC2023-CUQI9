b8011be35d7fecd6571cdb151ad74f459301262e
rasmuskleist <kleist.rasmus@gmail.com>
2023-10-30 11:40:43 +0100

    Loop over files in input folder

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def main():
     parser.add_argument("category", type=int)
     args = parser.parse_args()
 
+    # TODO: Fix stupid appending "/data*.mat"
+    files = glob.glob(args.input_folder + "/data*.mat")
+    files = sorted(files)
+    for idx, path in enumerate(files):
+        mat = sp.io.loadmat(path)
+
 
 if __name__ == "__main__":
     main()
