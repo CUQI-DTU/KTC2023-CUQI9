b870c6c742730305707c59b89489ef1526b5bd3c
rasmuskleist <kleist.rasmus@gmail.com>
2023-10-30 17:32:30 +0100

    Implement maksing function

--- ktc/data.py
+++ ktc/data.py
@@ -10,8 +10,20 @@ def _mean_free_potential(mpat, uel):
     return solve(a, b)
 
 
+def _electrode_mask(inj, drop):
+    _, n = inj.shape
+    mask = np.ones(inj.shape, dtype=bool)
+    for ii in range(n):
+        for jj in drop:
+            if inj[jj, ii] != 0:
+                mask[:, ii] = 0
+            mask[jj, :] = 0
+
+    return mask
+
+
 class Data:
-    def __init__(self, mat):
+    def __init__(self, mat, drop = []):
         # Load current patterns and injections
         mpat = mat["Mpat"]
         inj = mat["Inj"]
@@ -20,6 +32,7 @@ class Data:
         # Based on the dimensions of the current injections, reshape potentials
         uel = np.reshape(mat["Uel"], (m - 1, n), order="F")
 
+        self.mask = _electrode_mask(inj, drop)
         self.measurement_pattern = mpat.T
         self.current_injection = inj.T
         self.electrode_potential = _mean_free_potential(mpat.T, uel).T
@@ -28,7 +41,7 @@ class Data:
 
 
 class Reference(Data):
-    def __init__(self, mat):
+    def __init__(self, mat, drop = []):
         # Load current patterns and injections
         mpat = mat["Mpat"]
         injref = mat["Injref"]
@@ -37,6 +50,7 @@ class Reference(Data):
         # Based on the dimensions of the current injections, reshape potentials
         uelref = np.reshape(mat["Uelref"], (m - 1, n), order="F")
 
+        self.mask = _electrode_mask(injref, drop)
         self.measurement_pattern = mpat.T
         self.current_injection = injref.T
         self.electrode_potential = _mean_free_potential(mpat.T, uelref).T
