3c34593aa173df06d25ca94825d2ddf76dcb3451
jakobtkn <s194325@student.dtu.dk>
2023-10-31 12:00:22 +0100

    WIP. Grad caching. Problem with random meshes

--- ktc/reconstruction.py
+++ ktc/reconstruction.py
@@ -1,9 +1,10 @@
 import numpy as np
 from itertools import product
+from dolfin import Function, plot
+import h5py
 
 from ktc.model import FenicsForwardModel
 from ktc.smprior import SMPrior
-from dolfin import Function, plot
 
 class SeriesReversion:
     # Current injections: each row is a unique injection pattern
@@ -20,27 +21,36 @@ class SeriesReversion:
             self.U.append(Ui)
 
         self.W = W
-        gradient_file_name = "ktc/cache/grad_" + str(recon_mesh.num_cells()) + ".txt"
-        try:
-            self.gradient = np.loadtxt(gradient_file_name)
-        except OSError as error:
-            print("Generating new gradient")
+        
+        grad_cache_filename = "cache/gradient_cache.hdf5"
+        grad_cache = h5py.File(grad_cache_filename, "r")
+        grad_id = str(hash(recon_mesh))
+        print(grad_id)
+        if grad_id in grad_cache.keys():
+            print("Read gradient from cache.")
+            self.gradient = grad_cache.get(grad_id)
+        else:
+            print("No gradient found. Generating new gradient.")
             self.gradient = self._gradient()
-            np.savetxt(gradient_file_name, self.gradient)
+            grad_cache.close()
+            grad_cache = h5py.File(grad_cache_filename, "w")
+            grad_cache.create_dataset(grad_id, data = self.gradient)
+        grad_cache.close()
 
     def _gradient(self):
-        N = self.recon_mesh.num_cells()
+        # N = self.recon_mesh.num_cells()
 
-        blocks = []
-        for n in range(N):
-            P_list = []
-            chi = self.model.basis(n, self.W)
-            for ui in self.u:
-                _, P = self.model.solve_pertubation(chi, ui)
-                P_list.append(P)
-            blocks.append(P_list)
+        # blocks = []
+        # for n in range(N):
+        #     P_list = []
+        #     chi = self.model.basis(n, self.W)
+        #     for ui in self.u:
+        #         _, P = self.model.solve_pertubation(chi, ui)
+        #         P_list.append(P)
+        #     blocks.append(P_list)
 
-        return np.block(blocks).T
+        # return np.block(blocks).T
+        return np.zeros((10,10))
 
     def _smoothingPrior(self):
         sigma0 = np.ones((self.recon_mesh.num_vertices(), 1))  # linearization point
@@ -54,18 +64,19 @@ class SeriesReversion:
     def reconstruct(self, voltages):
         J = self.gradient
 
-        L = self._smoothingPrior()
-        # F1, _, _, _ = np.linalg.lstsq(J, (voltages - self.U).flatten(order = "F"))
-        F1 = np.linalg.solve(
-            J.T @ J + L.T @ L, J.T @ (voltages - self.voltages_ref).flatten(order="F")
-        )
+        # L = self._smoothingPrior()
+        # # F1, _, _, _ = np.linalg.lstsq(J, (voltages - self.U).flatten(order = "F"))
+        # F1 = np.linalg.solve(
+        #     J.T @ J + L.T @ L, J.T @ (voltages - self.voltages_ref).flatten(order="F")
+        # )
+        print(J)
         # u, _ = self.model.solve(current_injections)
 
         # h = -self.model.poisson(F1, current_injections, u)
         # v = self.model.poisson(F1, current_injections, h)
         # F2 = np.linalg.solve(J, v)
 
-        return F1
+        # return F1
 
     def solution_plot(self, pertubation):
         f = Function(self.W)
